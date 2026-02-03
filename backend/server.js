f1150e191c78449afeb94627aacffb55a67e3166
Ivo  Bryan <ivobryan12@gmail.com>
2026-02-03 10:20:51 +0200

    Added the socket and middleware for the socket.io

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,72 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import authRoutes from "./routes/Auth.routes.js";
 import connectDB from "./config/db.js";
-import protect from "./middleware/authMiddleware.js";
+import { Server } from "socket.io";
+import http from "http";
+import User from "./models/User.js";
+import { timeStamp } from "console";
 
 dotenv.config();
-const app = express();
+connectDB();
 
+const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+app.use("/api/auth", authRoutes);
+
 const PORT = process.env.PORT || 5000;
 
-app.use("/api/auth", authRoutes);
-connectDB();
+const server = http.createServer(app);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+const io = new Server(server, {
+  cors: "http://localhost:3000 ", // frontend
+  credentials: true,
+});
+
+io.use(async (socket, next) => {
+  try {
+    const cookies = cookieParser.parse(socket.handshake.headers.cookie || "");
+    const token = cookies.jwt;
+
+    if (!token) {
+      return next(new Error("Not authorized, no token"));
+    }
+
+    // Verify JWT
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Attach user to socket
+    const user = await User.findById(decoded.id).select("-password");
+    if (!user) {
+      return next(new Error("User not found"));
+    }
+
+    socket.user = user; // now every socket has its user
+    next();
+  } catch (err) {
+    next(new Error("Authentication failed"));
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("sendMessage", (data) => {
+    const message = {
+      sender: socket.user.username,
+      text: data.text,
+      timeStamp: new Date(),
+    };
+
+    io.emit("receiveMessage", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected", socket.id);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
